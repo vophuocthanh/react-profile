318795f9d3ce1a0847953e163145ff318fc6721d
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2023-05-20 12:28:53 +0700

    update

--- .history/src/components/banner/RightBanner_20230520122533.js
+++ .history/src/components/banner/RightBanner_20230520122533.js
@@ -6,7 +6,7 @@ const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
-        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10 rounded-lg object-cover"
+        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10 rounded-sm object-cover"
         src={avatar2}
         alt="bannerImg"
       />
